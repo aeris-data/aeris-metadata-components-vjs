2f370b189bfa4019839ce684a6d7c17b1674745a
lvu <le-hung.vu@obs-mip.fr>
2019-02-01 17:01:07 +0100

    fix(ui-table): css

--- src/lib/modules/aeris-metadata-ui/submodules/aeris-metadata-ui-table/components/aeris-metadata-ui-table.vue
+++ src/lib/modules/aeris-metadata-ui/submodules/aeris-metadata-ui-table/components/aeris-metadata-ui-table.vue
@@ -1,18 +1,16 @@
 <template>
-  <section data-aeris-metadata-table-layout>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="title in table.header" :key="title">{{ title }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(row, index) in table.content" :key="index">
-          <td v-for="(column, index) in row" :key="column.value + '_' + index"><span v-html="column.value" /></td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+  <table aeris-metadata-ui-table>
+    <thead>
+      <tr>
+        <th v-for="title in table.header" :key="title">{{ title }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, index) in table.content" :key="index">
+        <td v-for="(column, index) in row" :key="column.value + '_' + index"><span v-html="column.value" /></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -58,37 +56,37 @@ export default {
 </script>
 
 <style scoped>
-[data-aeris-metadata-table-layout] table {
+[aeris-metadata-ui-table] {
   display: flex;
   flex-direction: column;
   margin-bottom: 16px;
 }
 
-[data-aeris-metadata-table-layout] table th {
+[aeris-metadata-ui-table] th {
   text-align: left;
   font-weight: 300;
   font-size: 1.1rem;
 }
 
-[data-aeris-metadata-table-layout] table tbody tr:nth-child(odd) {
+[aeris-metadata-ui-table] tbody tr:nth-child(odd) {
   background: #eee;
 }
 
-[data-aeris-metadata-table-layout] table tr {
+[aeris-metadata-ui-table] tr {
   display: flex;
   padding: 10px;
 }
 
-[data-aeris-metadata-table-layout] table tr th,
-[data-aeris-metadata-table-layout] table tr td {
+[aeris-metadata-ui-table] tr th,
+[aeris-metadata-ui-table] tr td {
   width: 33%;
 }
 
-[data-aeris-metadata-table-layout] table td {
+[aeris-metadata-ui-table] td {
   word-break: break-all;
 }
 
-[data-aeris-metadata-table-layout] table td div {
+[aeris-metadata-ui-table] td div {
   display: flex;
 }
 </style>
