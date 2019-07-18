612fbeebfd68f1c993e267188b25774c174b9767
esteban.mercier <esteban.mercier@obs-mip.fr>
2019-07-18 11:09:58 +0200

    ui(blocks): adds icons

--- src/lib/modules/aeris-metadata-ui/submodules/aeris-metadata-layout/components/aeris-metadata-layout.vue
+++ src/lib/modules/aeris-metadata-ui/submodules/aeris-metadata-layout/components/aeris-metadata-layout.vue
@@ -1,7 +1,8 @@
 <template>
   <div :style="applyTheme" class="data-aeris-metadata-layout" data-template="metadata-block">
     <header v-if="showTitle">
-      <h3>{{ title }}</h3>
+      <h3><i :class="icon" /> {{ title }}</h3>
+      <div class="aeris-icon-group" />
     </header>
     <section :class="{ main: showTitle }"><slot /></section>
   </div>
@@ -13,6 +14,10 @@ export default {
 
   props: {
     title: {
+      type: String,
+      required: true
+    },
+    icon: {
       type: String,
       required: true
     },
@@ -80,46 +85,6 @@ export default {
 }
 
 .data-aeris-metadata-layout >>> header~article {
-  margin-left: 10px;
-}
-</style>
-.data-aeris-metadata-layout {
-  display: flex;
-  flex-direction: column;
-  border: none;
-}
-
-.data-aeris-metadata-layout header h5,
-.data-aeris-metadata-layout header h6 {
-  color: var(--primaryColor);
-}
-
-.data-aeris-metadata-layout header h3 {
-  border-bottom: 1px solid #CCC;
-  margin: 10px 0;
-  font-weight: 400;
-  color: #555;
-  font-family: Arial, sans-serif;
-  line-height: 1.7;
-}
-
-.data-aeris-metadata-layout .main {
-  padding: 16px 0 0 0px;
-}
-
-.data-aeris-metadata-layout h5 {
-  font-size: 1rem;
-  font-weight: 400;
-  margin: 0;
-}
-
-.data-aeris-metadata-layout h6 {
-  font-size: 0.9rem;
-  font-weight: 600;
-  margin: 0 0 15px 0;
-}
-
-.data-aeris-metadata-layout article {
   margin-left: 10px;
 }
 </style>
